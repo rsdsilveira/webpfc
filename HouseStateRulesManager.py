2f570af4c832e874315d084e23cd10eaa0ec3c36
Rafael Silveira <rsdsilveira@gmail.com>
2015-05-04 13:41:33 -0300

    Adicionada feature de árvore de decisao

--- HouseStateRulesManager.py
+++ HouseStateRulesManager.py
@@ -2,6 +2,10 @@ __author__ = 'Kaike'
 #from sklearn import svm
 #from Model import HouseState
 import DatabaseService
+import pickle
+import pandas as PandasLibrary
+from model import *
+from sklearn import tree
 
 class HouseStateRulesManager(object):
     def __init__(self):
@@ -11,8 +15,43 @@ class HouseStateRulesManager(object):
         rules = self.databaseService.get_house_states()
         self.databaseService.save_house_state_rules(self.new_rules_algotithm())
 
-    def new_rules_algotithm(self, history):
-        return list()
+    def new_rules_algotithm(self):
+        history = self.databaseService.get_house_states()
 
-    def getHouseStateRules(self):
-        return self.databaseService.getHouseStateRules()
+        roomNames = ["office", "bedroom"]
+        for roomName in roomNames:
+            devices = ["Light","Temperature","Curtain"]
+            for device in devices:
+                deviceName = roomName + device
+                treeResult = tree.DecisionTreeClassifier(criterion="entropy")
+                treeResult.fit(history.filter(regex= 'user|room|hour'), history.filter(regex= deviceName))
+                with open(deviceName + '.plk', 'wb') as f:
+                    pickle.dump(treeResult, f)
+
+    def getHouseStateRules(self, roomState):
+        roomName = "office" if roomState.room == 0 else "bedroom"
+        # na linha abaixo coloquei roomState[0].user, mas e se um roomState tem mais de um user?
+        decisionTreeEntries = [roomState[0].user,roomState.room, roomState.hour]
+        roomStateRuleResult = RoomStateRule()
+        
+        with open(roomName + "Light" + '.plk', 'rb') as f:
+            decisionTree = pickle.load(f)
+            roomStateRuleResult.light = decisionTree.Predict(decisionTreeEntries)[0]
+            f.close()
+
+        with open(roomName + "Temperature" + '.plk', 'rb') as f:
+            decisionTree = pickle.load(f)
+            roomStateRuleResult.temperature = decisionTree.Predict(decisionTreeEntries)[0]
+            f.close()
+            
+        with open(roomName + "Curtain" + '.plk', 'rb') as f:
+            decisionTree = pickle.load(f)
+            roomStateRuleResult.curtain = decisionTree.Predict(decisionTreeEntries)[0]
+            f.close()
+            
+        roomStateRuleResult.hour = roomState.hour
+        # na linha abaixo coloquei roomState[0].user, mas e se um roomState tem mais de um user?
+        roomStateRuleResult.user = roomState.user[0]
+        roomStateRuleResult.room = roomState.room
+        
+        return roomStateRuleResult            
