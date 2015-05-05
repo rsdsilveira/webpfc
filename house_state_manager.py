c6f275c36e338d947d3afaf41ddcf968f4a297f0
carloshpp <carloshpp.ime@gmail.com>
2015-05-05 15:00:41 -0300

    add format time method

--- house_state_manager.py
+++ house_state_manager.py
@@ -9,13 +9,15 @@ class HouseStateManager (object):
     def __init__(self):
         self.databaseService = database_service.DatabaseService
         self.devicesControl = devices_control.DevicesControl
-        self.save_current_office_state(room_state.RoomState("office", datetime.datetime.now(), 0, 0, 0, 0))
-        self.save_current_bedroom_state(room_state.RoomState("bedroom", datetime.datetime.now(), 0, 0, 0, 0))
+        self.save_current_office_state(room_state.RoomState("office", self.format_current_time(), 0, 0, 0, 0))
+        self.save_current_bedroom_state(room_state.RoomState("bedroom", self.format_current_time(), 0, 0, 0, 0))
 
     def save_current_office_state(self, houseState):
+        houseState.hour = self.format_current_time()
         SimpleCache().set("current_office_state", houseState)
 
     def save_current_bedroom_state(self, houseState):
+        houseState.hour = self.format_current_time()
         SimpleCache().set("current_bedroom_state", houseState)
 
     def get_current_house_state(self):
@@ -28,19 +30,15 @@ class HouseStateManager (object):
         return SimpleCache().get("current_bedroom_state")
 
     def save_house_state_in_db(self, houseState):
+        houseState.hour = self.format_current_time()
         self.databaseService.save_house_state(houseState)
 
 
-    #def format_time(self, time):
-    #    time.hour = datetime.datetime.now().hour
-    #    if datetime.datetime.now().minute > 30:
-    #        time.minute = 30
-    #    else:
-    #        time.minute = 0
-    #    time.second = 0
-    #
-    #    datetime.datetime.now().time()
-    #    return time
+    def format_current_time(self):
+        formatted_time = datetime.datetime.now()
+        if datetime.datetime.now().minute > 30:
+            formatted_time += 0.5
+        return formatted_time
 
     def change_office_temperature(self, new_temperature):
         office_state = self.get_current_office_state()
